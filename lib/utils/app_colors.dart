dbf7779280be1e4e25c5ef83e5ef9e6ab8d57a07
TharudiHS <tharudihimeshaaa@gmail.com>
2025-08-27 09:22:24 +0530

    added new app_colours.dart file

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const white = Color(0xFFFFFFFF);
+  static const red = Color(0xFFBD1F2D);
+  static const black = Color(0xFF000000);
+  static const blackish = Color(0xFF1D1D1B);
+  static const gray = Color(0xFF646464);
+  static const lightGray = Color(0xFF707070);
+  static const bgGray = Color(0xFFF2F2F2);
+  static const translucentDark = Color(0x2C273814);
+  static const darkRed = Color(0xFFAE313D);
+  static const deepRose = Color(0xFFA0414C);
+  static const mediumGray = Color(0xFFA8A8A8);
+  static const lightBorder = Color(0xFFDFDFDF);
+  static const offWhite = Color(0xFFEBFFFF);
+  static const softRed = Color(0xFFFF8489);
+  static const pinkish = Color(0xFFF8BEC0);
+  static const gray92 = Color(0xFF929292);
+  static const darkBlack = Color(0xFF221F1F);
+  static const darkGray = Color(0xFF575757);
+  static const veryLightBlue = Color(0xFFF7F9FC);
+  static const softGray = Color(0xFFF3F3F3);
+}
