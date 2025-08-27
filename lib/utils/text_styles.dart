026d0b91d9ff883b710ba88d631af029a3e202e6
TharudiHS <tharudihimeshaaa@gmail.com>
2025-08-27 10:10:08 +0530

    added text_styles.dart file and added new text styles

---
+++ lib/utils/text_styles.dart
@@ -0,0 +1,416 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class TextStyles {
+  static const TextStyle size45WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 45,
+    fontWeight: FontWeight.bold,
+    color: AppColors.blackish,
+    height: 1.3,
+  );
+  static const TextStyle size20Weightboldvisbycfmedium = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: AppColors.blackish,
+  );
+  static const TextStyle size32WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    color: AppColors.blackish,
+    height: 1.3,
+  );
+  static const TextStyle size16WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+  );
+  static const TextStyle size25WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 25,
+    fontWeight: FontWeight.bold,
+    color: AppColors.blackish,
+    height: 1.3,
+  );
+  static const TextStyle size16visbycfmedium = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 16,
+    color: AppColors.gray,
+  );
+  static const TextStyle size11Weightboldvisbycfmedium = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 11,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+  );
+  static const TextStyle size11Weightboldvisbycfmediumgray = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 11,
+    fontWeight: FontWeight.bold,
+    color: AppColors.gray,
+  );
+  static const TextStyle size25WeightboldvisbycfBoldblack = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 25,
+    fontWeight: FontWeight.bold,
+    color: AppColors.black,
+  );
+  static const TextStyle size25visbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 25,
+    color: AppColors.black,
+  );
+  static const TextStyle size15Weightboldvisbycfmediumgray = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 15,
+    fontWeight: FontWeight.bold,
+    color: AppColors.gray,
+  );
+  static const TextStyle size40visbycfmedium = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 40,
+    color: AppColors.white,
+  );
+  static const TextStyle size13visbycfregular = TextStyle(
+    fontFamily: 'visbycf-regular',
+    fontSize: 13,
+    color: AppColors.white,
+  );
+  static const TextStyle size18WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: AppColors.red,
+  );
+  static const TextStyle size22WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+  );
+  static const TextStyle size18Weightboldvisbycfregular = TextStyle(
+    fontFamily: 'visbycf-regular',
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: AppColors.darkBlack,
+  );
+  static const TextStyle size13Weightboldvisbycfregular = TextStyle(
+    fontFamily: 'visbycf-regular',
+    fontSize: 13,
+    fontWeight: FontWeight.bold,
+    color: AppColors.darkRed,
+  );
+  static const TextStyle size18WeightboldvisbycfBoldwhite = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+  );
+  static const TextStyle size48WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 48,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+    height: 1.2,
+  );
+  static const TextStyle size15visbycfmediumwhite = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 15,
+    color: AppColors.white,
+    height: 1.4,
+  );
+  static const TextStyle size15WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 15,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+  );
+  static const TextStyle size27visbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 27,
+    color: AppColors.black,
+  );
+  static const TextStyle size27WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 27,
+    fontWeight: FontWeight.bold,
+    color: AppColors.black,
+  );
+  static const TextStyle size25Weightboldvisbycfmedium = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 25,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+  );
+  static const TextStyle size14visbycfmedium = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 14,
+    color: AppColors.white,
+  );
+  static const TextStyle size12WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+  );
+  static const TextStyle size14visbycfmediumgray92 = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 14,
+    color: AppColors.gray92,
+  );
+  static const TextStyle size18WeightboldvisbycfBoldblack = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: AppColors.black,
+  );
+  static const TextStyle size15visbycfregular = TextStyle(
+    fontFamily: 'visbycf-regular',
+    fontSize: 15,
+    color: AppColors.gray92,
+  );
+  static const TextStyle size15visbycfregularred = TextStyle(
+    fontFamily: 'visbycf-regular',
+    fontSize: 15,
+    color: AppColors.red,
+  );
+  static const TextStyle size45WeightboldvisbycfBoldwhite = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 45,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+    height: 1.3,
+  );
+
+  static const TextStyle size15WeightboldvisbycfBoldblack = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 15,
+    color: AppColors.black,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size30Weightboldvisbycfregular = TextStyle(
+    fontFamily: 'visbycf-regular',
+    fontSize: 30,
+    color: AppColors.black,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size30visbycfregular = TextStyle(
+    fontFamily: 'visbycf-regular',
+    fontSize: 30,
+    color: AppColors.black,
+  );
+  static const TextStyle size18visbycfmedium = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 18,
+    color: AppColors.gray,
+    height: 1.4,
+  );
+  static const TextStyle size15visbycfregulargray92 = TextStyle(
+    fontFamily: 'visbycf-regular',
+    fontSize: 15,
+    color: AppColors.gray92,
+  );
+  static const TextStyle size13visbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 13,
+    color: AppColors.white,
+  );
+  static const TextStyle size15visbycfmedium = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 15,
+    color: AppColors.white,
+  );
+  static const TextStyle size14WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 14,
+    color: AppColors.white,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size14visbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 14,
+    color: AppColors.white,
+  );
+  static const TextStyle size19WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 19,
+    color: AppColors.white,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size20Weightboldvisbycfregular = TextStyle(
+    fontFamily: 'visbycf-regular',
+    fontSize: 20,
+    color: AppColors.white,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size13visbycfmedium = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 13,
+    color: Colors.white54,
+  );
+  static const TextStyle size20visbycfdemibold = TextStyle(
+    fontFamily: 'visbycf-demibold',
+    fontSize: 20,
+    color: AppColors.white,
+  );
+  static const TextStyle size25WeightboldvisbycfBoldwhite = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 25,
+    color: AppColors.white,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size16visbycfregular = TextStyle(
+    fontFamily: 'visbycf-regular',
+    fontSize: 20,
+    color: AppColors.white,
+  );
+  static const TextStyle size15WeightboldvisbycfBoldred = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 15,
+    color: AppColors.red,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size16visbycfmediumwhite = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 16,
+    color: AppColors.white,
+  );
+  static const TextStyle size28visbycfregular = TextStyle(
+    fontFamily: 'visbycf-regular',
+    fontSize: 28,
+    color: AppColors.black,
+  );
+  static const TextStyle size15visbycfregulargray = TextStyle(
+    fontFamily: 'visbycf-regular',
+    fontSize: 15,
+    color: AppColors.gray,
+  );
+  static const TextStyle size55WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 55,
+    color: AppColors.white,
+    fontWeight: FontWeight.bold,
+    height: 66 / 55,
+  );
+  static const TextStyle size16visbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 16,
+    color: AppColors.white,
+    height: 22 / 16,
+  );
+  static const TextStyle size14visbycfmediumblack = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 14,
+    color: AppColors.black,
+    height: 22 / 14,
+  );
+  static const TextStyle size14visbycfmediumBlack = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 14,
+    color: AppColors.black,
+    height: 22 / 14,
+  );
+  static const TextStyle size14visbycfmediumred = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 14,
+    color: AppColors.red,
+    height: 22 / 14,
+  );
+  static const TextStyle size16visbycfmediumblack = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 16,
+    color: AppColors.black,
+  );
+  static const TextStyle size13visbycfmediumgray92 = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 13,
+    color: AppColors.gray92,
+  );
+  static const TextStyle size45visbycfregular = TextStyle(
+    fontFamily: 'visbycf-regular',
+    fontSize: 45,
+    color: AppColors.white,
+  );
+  static const TextStyle size20visbycfmedium = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 20,
+    color: AppColors.white,
+  );
+  static const TextStyle size20visbycfmediumwhite = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 20,
+    color: AppColors.white,
+    height: 1.5,
+  );
+  static const TextStyle size55visbycfmedium = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 55,
+    color: AppColors.white,
+  );
+  static const TextStyle size55Weightboldvisbycfmedium = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 55,
+    color: AppColors.white,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size18visbycfmediumwhite = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 18,
+    color: AppColors.white,
+    height: 1.5,
+  );
+  static const TextStyle size20WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 20,
+    color: AppColors.black,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size20WeightboldvisbycfBoldwhite = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 20,
+    color: AppColors.white,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size55visbycfmediumblackish = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 55,
+    color: AppColors.blackish,
+  );
+  static const TextStyle size55Weightboldvisbycfmediumblackish = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 55,
+    color: AppColors.blackish,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size20Weightboldvisbycfmediumblackish = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 20,
+    color: AppColors.blackish,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size14visbycfmediumgrey = TextStyle(
+    fontFamily: 'visbycf-medium',
+    fontSize: 14,
+    color: Colors.grey,
+  );
+  static const TextStyle size40WeightboldvisbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 40,
+    color: AppColors.white,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size14WeightboldvisbycfBoldblack = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 14,
+    color: AppColors.black,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle size15visbycfBold = TextStyle(
+    fontFamily: 'visbycf-bold',
+    fontSize: 15,
+    color: AppColors.gray92,
+  );
+}
